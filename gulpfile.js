b6d3ce318c72104bec38aed0163647601756e607
stefaneg <gulli@sprettur.is>
2014-02-16 18:24:17 +0000

    Gulp running with livereload using ecstatic to serve static files

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,65 @@
-var gulp = require('gulp');  
-var browserify = require('gulp-browserify');  
-var concat = require('gulp-concat');  
-var styl = require('gulp-styl');  
-var refresh = require('gulp-livereload');  
-var lr = require('tiny-lr');  
-var server = lr();
-
-//We only configure the server here and start it only when running the watch task
-var server = express();
-//Add livereload middleware before static-middleware
-server.use(livereload({
-  port: livereloadport
-}));
-server.use(express.static('./build'));
-
-
-gulp.task('scripts', function() {  
-    gulp.src(['src/**/*.js'])
+var http = require('http');
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
+var less = require('gulp-less');
+var refresh = require('gulp-livereload');
+var lr = require('tiny-lr');
+var lrserver = lr();
+var minifyCSS = require('gulp-minify-css');
+var embedlr = require('gulp-embedlr');
+var ecstatic = require('ecstatic');
+var imagemin = require('gulp-imagemin');
+ 
+var livereloadport = 35729,
+    serverport = 5001;
+ 
+gulp.task('scripts', function() {
+    return gulp.src(['app/src/**/*.js'])
         .pipe(browserify())
         .pipe(concat('dest.js'))
-        .pipe(gulp.dest('build'))
-        .pipe(refresh(server))
+        .pipe(gulp.dest('dist/build'))
+        .pipe(refresh(lrserver));
+});
+ 
+gulp.task('styles', function() {
+    return gulp.src(['app/css/style.less'])
+        .pipe(less())
+        .on('error', console.log)
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('dist/build'))
+        .pipe(refresh(lrserver));
+});
+ 
+gulp.task('serve', function() {
+  //Set up your static fileserver, which serves files in the build dir
+  http.createServer(ecstatic({ root: __dirname + '/dist' })).listen(serverport);
+ 
+  //Set up your livereload server
+  lrserver.listen(livereloadport);
+});
+ 
+ 
+gulp.task('html', function() {
+    return gulp.src("app/*.html")
+        .pipe(embedlr())
+        .pipe(gulp.dest('dist/'))
+        .pipe(refresh(lrserver));
 })
-
-gulp.task('styles', function() {  
-    gulp.src(['css/**/*.css'])
-        .pipe(styl({compress : true}))
-        .pipe(gulp.dest('build'))
-        .pipe(refresh(server))
-})
-
-gulp.task('lr-server', function() {  
-    server.listen(35729, function(err) {
-        if(err) return console.log(err);
-    });
-})
-
-gulp.task('default', function() {  
-    gulp.run('lr-server', 'scripts', 'styles');
-
-    gulp.watch('src/**', function(event) {
-        gulp.run('scripts');
-    })
-
-    gulp.watch('css/**', function(event) {
-        gulp.run('styles');
-    })
-})
+ 
+gulp.task('assets', function() {
+    return gulp.src("app/assets/**")
+        .pipe(imagemin({optimizationLevel: 5}))
+        .pipe(gulp.dest('dist/assets/'))
+        .pipe(refresh(lrserver));
+});
+ 
+// Requires gulp >=v3.5.0
+gulp.task('watch', function () {
+    gulp.watch('app/src/**', ['scripts']);
+    gulp.watch('app/css/**', ['styles']);
+    gulp.watch('app/**/*.html', ['html']);
+    gulp.watch('app/assets/**', ['assets']);
+});
+ 
+gulp.task('default', ['scripts', 'styles', 'html', 'assets', 'serve', 'watch']);
